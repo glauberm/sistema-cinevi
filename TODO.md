13de12163df796db1bcffb832cb1bae004334e83
Glauber Mota <glaubernm@gmail.com>
2017-08-30 04:59:28 -0300

    Melhorias no CSS, especialmente cores; Cores diferentes se a reserva é sua.

--- TODO.md
+++ TODO.md
@@ -1,3 +1,1 @@
 # TO-DO
-
-- Cores no calendário somente para reservas criadas pelo atual usuário.
--- src/Cinevi/AlmoxarifadoBundle/Listener/LoadDataListener.php
+++ src/Cinevi/AlmoxarifadoBundle/Listener/LoadDataListener.php
@@ -32,17 +32,24 @@ class LoadDataListener
         // Pega os eventos
         $reservas = $this->em->getRepository('CineviAlmoxarifadoBundle:CalendarEvent')->findAll();
 
+        // Pega o user atual
+        $userAtual = $this->container->get('security.context')->getToken()->getUser();
+
         // Constrói no calendário
         foreach($reservas as $reserva)
         {
             $event = new Event();
 
+            // Constrói um array dos equipamentos e coloca no título
             $equipamentos = array();
+
             foreach( $reserva->getEquipamentos() as $equipamento ) {
                 $equipamentos[] = '['.$equipamento->getCodigo().'] '.$equipamento->getNome();
             }
 
             $event->setTitle( $reserva->getTitle()." (".$reserva->getUser()->getUsername().") - ".implode(", ", $equipamentos) );
+
+            // Data de Início e Fim
             $event->setStartDate($reserva->getStartDate());
             $event->setEndDate($reserva->getEndDate()->add(new \DateInterval('P1D')));
 
@@ -51,6 +58,13 @@ class LoadDataListener
                 'id' => $reserva->getId(),
             ));
             $event->setUrl($url);
+
+            // Classe se for user atual ou não
+            if($reserva->getUser() == $userAtual) {
+                $event->setClassName('reserva reserva-propria');
+            } else {
+                $event->setClassName('reserva');
+            }
 
             $calendarEvent->addEvent($event);
         }
